8d5522cf483cb043eb8c393827c3caaa47503f17
krutarth0 <krutarthzero@hotmail.com>
2021-03-08 23:11:50 +0530

    auth done

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Profile.module.scss'
 import { useRouter } from 'next/router';
 import {useAuth} from "../hooks/use-auth"
+import Link from 'next/link'
 
 export default function profile() {
     const router = useRouter();
@@ -13,15 +14,22 @@ export default function profile() {
     }
 
     if(!auth.user){
-        router.replace("/login")
+        // router.replace("/login")
         return (
-            <></>
+            <>
+            <h2>
+            It seems you are not authorized to view this page, 
+            <span style={{borderBottom:"2px solid black"}}><Link href="/login" >Sign In </Link> </span> 
+            
+            to explore more!
+            </h2>
+            </>
         )
     }
     return (
         <div className={styles.profileHeader}>
             <h1>
-                welcome...user 
+                welcome...{auth.user.email}
             </h1>
         </div>
     )
